43693884e92b1065433e86d59eede4c0a87f51f8
joshflutterwork <joshuaevanmannaim@gmail.com>
2022-08-30 22:37:30 +0700

    uploade last

--- lib/Widget/build_fantasy_list.dart
+++ lib/Widget/build_fantasy_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fugi_movie_app_team5/View/trendingScreen/detail_screen.dart';
 import 'package:intl/intl.dart';
 
 import '../Controller/riverpod/fantasy_riverpod.dart';
@@ -23,30 +24,42 @@ class BuildFantasyList extends ConsumerWidget {
             crossAxisCount: 2,
           ),
           itemBuilder: (context, index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  width: 120,
-                  height: 180,
-                  margin: const EdgeInsets.only(bottom: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                          'https://image.tmdb.org/t/p/w500${data[index].posterPath}',
-                        ),
-                        fit: BoxFit.cover),
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      id: data[index].id,
+                    ),
                   ),
-                ),
-                Text(
-                  "${data[index].originalTitle} (${DateFormat('yyyy').format(data[index].releaseDate)})",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                );
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 180,
+                    margin: const EdgeInsets.only(bottom: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            'https://image.tmdb.org/t/p/w500${data[index].posterPath}',
+                          ),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Text(
+                    "${data[index].originalTitle} (${DateFormat('yyyy').format(data[index].releaseDate)})",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             );
           },
         );
--- lib/Widget/build_tv_series_list.dart
+++ lib/Widget/build_tv_series_list.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 
 import '../Controller/riverpod/tv_series_riverpod.dart';
 import '../Model/tv_series/tv_series.dart';
+import '../View/trendingScreen/detail_screen.dart';
 
 class BuildTvSeriesList extends ConsumerWidget {
   const BuildTvSeriesList({
@@ -23,30 +24,42 @@ class BuildTvSeriesList extends ConsumerWidget {
             crossAxisCount: 2,
           ),
           itemBuilder: (context, index) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  width: 120,
-                  height: 180,
-                  margin: const EdgeInsets.only(bottom: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                          'https://image.tmdb.org/t/p/w500${data[index].posterPath}',
-                        ),
-                        fit: BoxFit.cover),
+            return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      id: data[index].id,
+                    ),
                   ),
-                ),
-                Text(
-                  "${data[index].originalName} (${DateFormat('yyyy').format(data[index].firstAirDate)})",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                );
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 180,
+                    margin: const EdgeInsets.only(bottom: 5),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                          image: NetworkImage(
+                            'https://image.tmdb.org/t/p/w500${data[index].posterPath}',
+                          ),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Text(
+                    "${data[index].originalName} (${DateFormat('yyyy').format(data[index].firstAirDate)})",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             );
           },
         );
